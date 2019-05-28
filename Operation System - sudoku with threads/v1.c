be39c71b5ba64923bb1298b151448fb1c175b6fb
aviv tfilin <avivt193@gmail.com>
2019-05-28 08:57:04 +0300

    Create v1.c

---
+++ Operation System - sudoku with threads/v1.c
@@ -0,0 +1,25 @@
+/*
+ * v1.c
+ *
+ *  Created on: May 19, 2019
+ *      Author: yevgeni
+ */
+
+#include "hw2.h"
+#include "function.h"
+
+int main(int argc, char **argv) {
+
+	int** sudoku;
+	if (argc == 1)
+		sudoku = createMatrix();
+
+	else {
+
+		sudoku = read_From_File(argv[1]);
+	}
+
+	threads_without_mutex(sudoku);
+
+	return 1;
+}
